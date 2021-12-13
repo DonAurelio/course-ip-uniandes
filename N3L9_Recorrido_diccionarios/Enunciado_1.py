2db4715dad04547cda60366ad2b0d42966a01052
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2021-12-13 11:12:07 -0500

    Update

---
+++ N3L9_Recorrido_diccionarios/Enunciado_1.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+NIVEL 3. LECCIÓN 9. RECORRIDO DE DICIONARIOS 
+Archivo: Enunciado_1.py
+Autor: Aurelio Vivas
+Fecha: Diciembre 13 de 2021
+"""
+
+"""
+PUNTO 1.
+
+# Escriba su respuesta aquí ...
+"""
+
+"""
+PUNTO 2.
+
+# Escriba su respuesta aquí ...
+"""
+
+"""
+PUNTO 3.
+
+# Escriba su respuesta aquí ...
+"""
+
+# BASE DE DATOS DE PRODUCTOS
+ventas = {
+    “manzana”: 55,
+    “pera”: 85,
+    “fresa”: 130,
+}
+
+precios = {
+    “manzana”: 500,
+    “pera”: 600,
+    “fresa”: 200,
+}
+
+"""
+PUNTO 4.
+"""
+
+
+# Escriba la función ....
+# Pruebe la función aquí ...
+
+
+"""
+PUNTO 5.
+"""
+
+
+# Escriba la función ....
+# Pruebe la función aquí ...
+
+
+"""
+PUNTO 6.
+"""
+
+
+# Escriba la función ....
+# Pruebe la función aquí ...
---
+++ N3L9_Recorrido_diccionarios/Enunciado_2.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+NIVEL 3. LECCIÓN 9. RECORRIDO DE DICIONARIOS
+Archivo: Enunciado_2.py
+Autor: Aurelio Vivas
+Fecha: Diciembre 13 de 2021
+"""
+
+"""
+PUNTO 1.
+"""
+
+# NUEVA BASE DE DATOS DE PRODUCTOS
+sucursales = {
+    "Cali":[ 
+         { "nombre": "manzana", "precio":15, "ventas": 2 },
+
+     ],
+    "Bogota": [
+
+
+
+    ],
+    "Medellin": [
+
+
+
+   ],
+}
+
+"""
+PUNTO 2.
+
+(Entiendo el problema) 
+Entradas:
+Salida:
+
+(Divide y Vencerás) 
+- Paso 1 ..
+- Paso 2 ..
+- Paso N ..
+
+(Código en Python)
+"""
+
+
+# Escriba la función ....
+# Pruebe la función aquí ...
+
+
+"""
+PUNTO 3.
+
+(Entiendo el problema) 
+Entradas:
+Salida:
+
+(Divide y Vencerás) 
+- Paso 1 ..
+- Paso 2 ..
+- Paso N ..
+
+(Código en Python)
+"""
+
+# Escriba la función ....
+# Pruebe la función aquí ...
+
